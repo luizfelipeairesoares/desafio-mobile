8ca5dd45ef5653f7c34b2a7fe7ae9f813dfe0325
luizfelipeairesoares <lfelipeas@gmail.com>
2017-02-05 18:17:30 -0200

    Adicionando extensão para UIView

---
+++ desafio-mobile/desafio-mobile/Utils/Extension/UIViewExtension.swift
@@ -0,0 +1,59 @@
+//
+//  UIViewExtension.swift
+//  desafio-mobile
+//
+//  Created by Luiz Aires Soares on 24/01/17.
+//  Copyright © 2017 Stone. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+}
